3d8a2db647dd8003c01c0933ed88a37ed930c1a6
mohan <mohan@DESKTOP-VTBDI4C>
2020-05-28 10:34:33 +0530

    Initial Commit Spring boot backend application

---
+++ CodeBoardSpecialities/src/main/java/com/codeboard/CodeBoardSpecialities/CodeBoardSpecialitiesApplication.java
@@ -0,0 +1,13 @@
+package com.codeboard.CodeBoardSpecialities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBoardSpecialitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBoardSpecialitiesApplication.class, args);
+	}
+
+}
---
+++ CodeBoardSpecialities/src/test/java/com/codeboard/CodeBoardSpecialities/CodeBoardSpecialitiesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeboard.CodeBoardSpecialities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeBoardSpecialitiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
